5e2dc760bcf979c34f3541556d40f0cd6aff99da
Iris Rifold <irisrifold@gmail.com>
2021-05-24 16:25:54 +0300

    skills

--- src/cmp/Skills/Skills.js
+++ src/cmp/Skills/Skills.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { CvContext } from "../../contexts/cvContext";
 import { Skill } from "../Skill/Skill";
-import { SocialIcon } from "react-social-icons";
 import "./Skills.scss";
 
 export function Skills() {
@@ -9,15 +8,6 @@ export function Skills() {
 
 	return (
 		<section className="skills flex">
-			{/* <div className="social-icons">
-				<SocialIcon
-					bgColor="black"
-					url="https://www.linkedin.com/in/iris-rifold/"></SocialIcon>
-				<SocialIcon
-					bgColor="black"
-					url="https://github.com/irisri"></SocialIcon>
-			</div> */}
-
 			<h2 className="text-color">Skills:</h2>
 			<div className="flex">
 				{skills.map((skill, index) => (
